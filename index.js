999e314cba952b59cfa96bc47e2feeb454ab1b4b
Agustin <agusscaramello@gmail.com>
2023-09-01 22:03:32 -0300

    Repo nuevo

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
-'use sctrict'
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
 
-require('dotenv').config({ path: `${process.cwd()}/.env` })
-const { app } = require('./app')
-const sequelize = require('./app/db')
-
-// Try connection
-const connectToDatabase = async () => {
-	try {
-		console.log('Prepare to create connection database ...')
-		await sequelize.sync()
-		console.log('Success connected to database')	
-	} catch(err) { 
-		console.log('Something wrong from connection : ', err)
-		process.exit(1)
-	}
-}
-
-const init = async () => {
-	await connectToDatabase()
-
-	// Running server
-	const PORT = process.env.PORT || 8000
-	app.listen(PORT, err => {
-
-		if (!err) console.log(`[${process.env.AUTHOR} - ${process.env.APP_NAME}] Server success running with PostgreSQL at port ${PORT}`)
-		else console.log('Something was wrong on server connection : ', err)
-	})
-}
-
-init()
+// Syncing all the models at once.
+conn.sync({ force: true }).then(() => {
+  server.listen(3001, () => {
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  });
+});
