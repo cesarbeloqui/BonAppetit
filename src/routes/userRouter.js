8fe449a231ce6c117d58e95a57964fc0c76df156
cesarbeloqui <120270198+cesarbeloqui@users.noreply.github.com>
2023-09-09 21:20:39 -0300

    ruta getAllUsers terminada

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 const { createUser, deleteUser } = require("../controllers/createUser");
 const { changePasswordUser } = require("../handlers/handlerCreateUser");
 const getUserById = require("../controllers/getUserById");
+const getAllUsers = require("../controllers/getAllUsers");
 
 const userRouter = Router();
 
 userRouter.get("/:id", getUserById);
+userRouter.get("/", getAllUsers);
 userRouter.post("/create", createUser);
 
 userRouter.delete("/delete", deleteUser);
