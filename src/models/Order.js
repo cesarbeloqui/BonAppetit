726b1ea528ae2bfceb8cbd25d49c8dd19203c3f2
Ignacio Bruna <joseignaciobruna95@gmail.com>
2023-09-15 20:36:11 -0300

    Correcion a modelos

--- src/models/Order.js
+++ src/models/Order.js
@@ -31,7 +31,6 @@ module.exports = (sequelize) => {
     userEmail: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
       validation: {
         isEmail: true
       }
--- src/models/User.js
+++ src/models/User.js
@@ -23,7 +23,7 @@ module.exports = (sequelize) => {
         },
       },
       role: {
-        type: DataTypes.ENUM("Client", "Admin", "Managers"),
+        type: DataTypes.ENUM("Client", "Admin", "Manager"),
         allowNull: false,
         defaultValue: "Client",
       },
