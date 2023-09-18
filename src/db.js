30c4125c4aa6d0131bf87ee7da789de66e0b60b9
cesarbeloqui <120270198+cesarbeloqui@users.noreply.github.com>
2023-09-18 18:50:18 -0300

    algo

--- src/db.js
+++ src/db.js
@@ -5,8 +5,9 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_VIRTUAL } = process.env;
 
 const sequelize = new Sequelize(
-  /* `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/resto`
-   ||   */DB_VIRTUAL, {
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/resto`
+   ||  DB_VIRTUAL,
+  {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
