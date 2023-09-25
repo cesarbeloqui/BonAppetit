dd78fd701a85dbf430311ab5edfc74b4110d8064
cesarbeloqui <120270198+cesarbeloqui@users.noreply.github.com>
2023-09-25 19:17:42 -0300

    respuesta users en update

--- src/controllers/changeTypeUser.js
+++ src/controllers/changeTypeUser.js
@@ -9,9 +9,11 @@ const changeTypeUser = async (req, res) => {
       return res.status(404).json("El usuario no fue encontrado");
     }
     user.role = role;
+
     await user.save();
+    const users = await User.findAll();
     console.log("El tipo de usuario ha sido cambiado correctamente");
-    res.status(200).json(user);
+    res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
